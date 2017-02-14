6183330cdd19625b14ca5cb5e1d6a71ad77df2ed
pmh118 <1104636h@student.gla.ac.uk>
2017-02-14 09:43:31 +0000

    new first commit

---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+# Create your views here.
+
+def about(request):
+    return HttpResponse("rango says here is the about page. <a href='/rango'>Index</a>")
